91a6d272987715d1d00bff6af41ec3fc69669bc3
klai01-su <klai01@syr.edu>
2024-12-13 17:34:22 -0500

    recommit for testing fails?

--- code/etl.py
+++ code/etl.py
@@ -1,20 +1,17 @@
 import pandas as pd
 import streamlit as st 
 
-
 def top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     amount_by_location_df = violations_df.pivot_table(index='location', values='amount', aggfunc='sum').sort_values(by='amount', ascending=False)
     amount_by_location_df['location'] = amount_by_location_df.index
     amount_by_location_df.reset_index(drop=True, inplace=True)
     return amount_by_location_df[amount_by_location_df['amount'] >= threshold]
 
-
 def top_locations_mappable(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     top_locations_df = top_locations(violations_df, threshold)
     violations_pre_merge_df = violations_df[['location', 'lat', 'lon']].drop_duplicates(subset=['location'])
     merged_df = pd.merge(top_locations_df, violations_pre_merge_df, on='location')
     return merged_df.drop_duplicates()
-
 
 def tickets_in_top_locations(violations_df : pd.DataFrame, threshold=1000)  -> pd.DataFrame:
     top_locations_df = top_locations(violations_df)
